34489a486644f8e319fdbb71a3fe90725e48ba51
dawn <90008@gaze.systems>
2026-02-06 08:28:25 +0300

    [nix] update hashes

--- default.nix
+++ default.nix
@@ -20,6 +20,7 @@ rustPlatform.buildRustPackage {
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
+      "rmp-0.8.15" = "sha256-0VATbSR2lGiCJ8Ww4a5pkOHSRUjoysnFonpKS/oMzgU=";
       "jacquard-0.9.5" = "sha256-3bVpFW/qtP9cjkU2LflHjSI3mzw1iUPH+92n0zcOqDg=";
     };
   };
