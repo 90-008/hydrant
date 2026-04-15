34fd8c89da1ff5e57d1d246e9cc2a7fd82169334
dawn <90008@gaze.systems>
2026-04-15 04:38:23 +0300

    [build] add rust toolchain toml and update flake to use nci for devshell

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   inputs.parts.url = "github:hercules-ci/flake-parts";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nci.url = "github:90-008/nix-cargo-integration";
 
   outputs =
     inp:
     inp.parts.lib.mkFlake { inputs = inp; } {
       systems = [ "x86_64-linux" ];
+      imports = [inp.nci.flakeModule];
       perSystem =
         {
           pkgs,
@@ -13,27 +15,29 @@
           ...
         }:
         {
-          packages.default = pkgs.callPackage ./default.nix {};
-          devShells = {
-            default = pkgs.mkShell {
-              packages = with pkgs; [
-                rustPlatform.rustLibSrc
-                rust-analyzer
-                cargo
-                cargo-outdated
-                rustc
-                rustfmt
-                gemini-cli
-                go
-                cmake
-                websocat
-                http-nu
-                clang
-                wild
-                psmisc
-              ];
-            };
+          nci.projects."hydrant" = {
+            path = ./.;
+            export = false;
           };
+          packages.default = pkgs.callPackage ./default.nix {};
+          devShells.default = config.nci.outputs."hydrant".devShell.overrideAttrs (old: {
+            packages = (old.packages or []) ++ (with pkgs; [
+              rustPlatform.rustLibSrc
+              rust-analyzer
+              cargo
+              cargo-outdated
+              rustc
+              rustfmt
+              gemini-cli
+              go
+              cmake
+              websocat
+              http-nu
+              clang
+              wild
+              psmisc
+            ]);
+          });
         };
     };
 }
