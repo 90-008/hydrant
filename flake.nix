ed6c83fd366236281a72db794bde6b6019807e7c
dawn <90008@gaze.systems>
2026-04-20 12:42:31 +0300

    [docs] add building and deployment

--- flake.nix
+++ flake.nix
@@ -16,12 +16,38 @@
           inputs',
           ...
         }:
+        let
+          buildDocs = pkgs.writeShellApplication {
+            name = "build-docs";
+            runtimeInputs = [ inputs'.verbiage.packages.build ];
+            text = ''
+              VERBIAGE_DATA="$(realpath "''${1:-$(pwd)/docs}")"
+              export VERBIAGE_DATA
+              VERBIAGE_TITLE="hydrant"
+              export VERBIAGE_TITLE
+              out="''${2:-$(pwd)/docs-dist}"
+              verbiage-build docs "$out"
+              printf '/static/* /static/:splat 200!\n/docs/w/* /:splat 301!\n/ /docs/w/~ 200!\n/* /docs/w/:splat 200!\n' > "$out/_redirects"
+            '';
+          };
+          deployDocs = pkgs.writeShellApplication {
+            name = "deploy-docs";
+            runtimeInputs = [ buildDocs pkgs.bun ];
+            text = ''
+              out="$(pwd)/docs-dist"
+              build-docs "$(pwd)/docs" "$out"
+              bunx wispctl -y --path "$out" --site hydrant-docs did:plc:dfl62fgb7wtjj3fcbb72naae
+            '';
+          };
+        in
         {
           nci.projects."hydrant" = {
             path = ./.;
             export = false;
           };
           packages.default = pkgs.callPackage ./default.nix {};
+          apps.build-docs = { type = "app"; program = "${buildDocs}/bin/build-docs"; };
+          apps.deploy-docs = { type = "app"; program = "${deployDocs}/bin/deploy-docs"; };
           devShells.default = config.nci.outputs."hydrant".devShell.overrideAttrs (old: {
             packages = (old.packages or []) ++ (with pkgs; [
               rustPlatform.rustLibSrc
